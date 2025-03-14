f2da1b2614ff83dec129513841f9163e744f9ffc
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-03-14 17:18:38 -0500

    Use router de next/naviagtion

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,25 @@
-export const metadata = {
-    title: "About Page"
+"use client";
+import { useRouter } from "next/navigation";
+
+export const meta = {
+    title: "About",
+    description: "Pagina de about"
 }
 
 function AboutPage (){
-    return <h1> Pagina de about </h1>
+    const router = useRouter();
+    return <section>
+        <h1> Pagina de about </h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sequi quidem ratione eos possimus molestias repellat ducimus necessitatibus facere. Dolores soluta dicta laudantium commodi amet alias vitae quisquam saepe possimus.</p>
+        <button className="bg-sky-400 px-3 py-2 rounded-md" onClick={
+            () => {
+                console.log('Ejecutando codigo antes de redireccionar')
+                router.push('/')
+            }
+        } >
+             Regresar
+        </button>    
+    </section>
 }
 
 export default AboutPage;
